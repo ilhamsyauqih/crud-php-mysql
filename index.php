c3d332f4b14fada8319d2e9c2d2b75b9970749f7
ilham-sh <ilhamsyauqih@outlook.co.id>
2025-10-08 20:14:52 +0700

    Menyelesaikan bagian html

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "cobamahasiswa";
+$host       = "localhost";
+$user       = "root";
+$pass       = "";
+$db         = "cobamahasiswa";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi) { //Cek Koneksi
     die("Tidak bisa terkoneksi ke database");
 }
+
+$nim        = "";
+$nama       = "";
+$alamat     = "";
+$fakultas   = "";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,14 +58,17 @@ if (!$koneksi) { //Cek Koneksi
                         <label for="fakultas" class="form-label">Fakultas</label>
                         <select class="form-control" name="fakultas" id="fakultas">
                             <option value="">- Pilih Fakultas -</option>
-                            <option value="ilkom">Ilmu Komputer</option>
-                            <option value="ilkes">Ilmu Kesehatan</option>
-                            <option value="hukum">Hukum</option>
-                            <option value="manajemen">Manajemen</option>
-                            <option value="ekonomi">Ekonomi</option>
-                            <option value="bisnis">Bisnis</option>
-                            <option value="pertanian">Pertanian</option>
+                            <option value="ilkom" <?php if($fakultas == "ilkom") echo "selected" ?>>Ilmu Komputer</option>
+                            <option value="ilkes" <?php if($fakultas == "ilkes") echo "selected" ?>>Ilmu Kesehatan</option>
+                            <option value="hukum"<?php if($fakultas == "hukum") echo "selected" ?>>Hukum</option>
+                            <option value="manajemen"<?php if($fakultas == "manajemen") echo "selected" ?>>Manajemen</option>
+                            <option value="ekonomi"<?php if($fakultas == "ekonomi") echo "selected" ?>>Ekonomi</option>
+                            <option value="bisnis"<?php if($fakultas == "bisnis") echo "selected" ?>>Bisnis</option>
+                            <option value="pertanian"<?php if($fakultas == "pertanian") echo "selected" ?>>Pertanian</option>
                         </select>
+                    </div>
+                    <div class="col-12">
+                        <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary">
                     </div>
                 </form>
             </div>
