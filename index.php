89ef0553c03358fc318efabc9d02d00427bd5077
ilham-sh <ilhamsyauqih@outlook.co.id>
2025-11-18 09:01:38 +0700

    menambahkan proses create

--- index.php
+++ index.php
@@ -13,6 +13,27 @@ $nim        = "";
 $nama       = "";
 $alamat     = "";
 $fakultas   = "";
+$sukses     = "";
+$error      = "";
+
+if (isset($_POST['simpan'])) {
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $fakultas = $_POST['fakultas'];
+
+    if ($nim && $nama && $alamat && $fakultas) {
+        $sql1 = "insert into mahasiswa(nim, nama, alamat, fakultas) values ('$nim', '$nama', '$alamat', '$fakultas')";
+        $q1  = mysqli_query($koneksi, $sql1);
+        if ($q1) {
+            $sukses = "Berhasil memasukkan data baru";
+        } else {
+            $error = "Gagal memasukkan data";
+        }
+    } else {
+        $error = "Silahkan masukkan data!";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -41,6 +62,24 @@ $fakultas   = "";
                 Buat / Edit Data
             </div>
             <div class="card-body">
+                <?php
+                if ($error) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $error ?>
+                    </div>
+                <?php
+                }
+                ?>
+                <?php
+                if ($sukses) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo $sukses ?>
+                    </div>
+                <?php
+                }
+                ?>
                 <form action="" method="POST">
                     <div class="mb-3">
                         <label for="nim" class="form-label">NIM</label>
@@ -58,13 +97,13 @@ $fakultas   = "";
                         <label for="fakultas" class="form-label">Fakultas</label>
                         <select class="form-control" name="fakultas" id="fakultas">
                             <option value="">- Pilih Fakultas -</option>
-                            <option value="ilkom" <?php if($fakultas == "ilkom") echo "selected" ?>>Ilmu Komputer</option>
-                            <option value="ilkes" <?php if($fakultas == "ilkes") echo "selected" ?>>Ilmu Kesehatan</option>
-                            <option value="hukum"<?php if($fakultas == "hukum") echo "selected" ?>>Hukum</option>
-                            <option value="manajemen"<?php if($fakultas == "manajemen") echo "selected" ?>>Manajemen</option>
-                            <option value="ekonomi"<?php if($fakultas == "ekonomi") echo "selected" ?>>Ekonomi</option>
-                            <option value="bisnis"<?php if($fakultas == "bisnis") echo "selected" ?>>Bisnis</option>
-                            <option value="pertanian"<?php if($fakultas == "pertanian") echo "selected" ?>>Pertanian</option>
+                            <option value="ilkom" <?php if ($fakultas == "ilkom") echo "selected" ?>>Ilmu Komputer</option>
+                            <option value="ilkes" <?php if ($fakultas == "ilkes") echo "selected" ?>>Ilmu Kesehatan</option>
+                            <option value="hukum" <?php if ($fakultas == "hukum") echo "selected" ?>>Hukum</option>
+                            <option value="manajemen" <?php if ($fakultas == "manajemen") echo "selected" ?>>Manajemen</option>
+                            <option value="ekonomi" <?php if ($fakultas == "ekonomi") echo "selected" ?>>Ekonomi</option>
+                            <option value="bisnis" <?php if ($fakultas == "bisnis") echo "selected" ?>>Bisnis</option>
+                            <option value="pertanian" <?php if ($fakultas == "pertanian") echo "selected" ?>>Pertanian</option>
                         </select>
                     </div>
                     <div class="col-12">
